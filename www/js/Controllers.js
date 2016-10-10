94d25c84716b2a3e028bf260e0b798db00ad6c12
Umang1996 <zheka_hockyman@ukr.net>
2016-10-10 15:47:02 +0300

    Баги с изображениями, переход по ссылкам

--- www/js/Controllers.js
+++ www/js/Controllers.js
@@ -31,12 +31,12 @@ angular.module('fp_controllers', [])
 
   })
   .controller('noviceController', function ($scope) {
-    $scope.beginnerPictures = [{time: 3000, src: '/img/noob/1.png'},
-      {time: 1000, src: '/img/noob/2.png'},
-      {time: 2000, src: '/img/noob/3.png'},
-      {time: 3000, src: '/img/noob/4.png'},
-      {time: 3000, src: '/img/noob/5.png'},
-      {time: 4000, src: '/img/noob/6.png'}
+    $scope.beginnerPictures = [{time: 3000, src: 'img/noob/1.png'},
+      {time: 1000, src: 'img/noob/2.png'},
+      {time: 2000, src: 'img/noob/3.png'},
+      {time: 3000, src: 'img/noob/4.png'},
+      {time: 3000, src: 'img/noob/5.png'},
+      {time: 4000, src: 'img/noob/6.png'}
     ];
     $scope.id = 0;
     $scope.beginnerImg = $scope.beginnerPictures[$scope.id].src;
@@ -58,17 +58,17 @@ $scope.id=0-1
 
   })
   .controller('middleController', function ($scope) {
-    $scope.MiddlePictures = [{time: 3000, src: '/img/middle/1.png'},
-      {time: 1000, src: '/img/middle/2.png'},
-      {time: 2000, src: '/img/middle/3.png'},
-      {time: 3000, src: '/img/middle/4and8.png'},
-      {time: 3000, src: '/img/middle/5.png'},
-      {time: 4000, src: '/img/middle/6.png'},
-      {time: 4000, src: '/img/middle/7.png'},
-      {time: 4000, src: '/img/middle/9.png'},
-      {time: 4000, src: '/img/middle/10.png'},
-      {time: 4000, src: '/img/middle/11.png'},
-      {time: 4000, src: '/img/middle/12.png'},
+    $scope.MiddlePictures = [{time: 3000, src: 'img/middle/1.png'},
+      {time: 1000, src: 'img/middle/2.png'},
+      {time: 2000, src: 'img/middle/3.png'},
+      {time: 3000, src: 'img/middle/4and8.png'},
+      {time: 3000, src: 'img/middle/5.png'},
+      {time: 4000, src: 'img/middle/6.png'},
+      {time: 4000, src: 'img/middle/7.png'},
+      {time: 4000, src: 'img/middle/9.png'},
+      {time: 4000, src: 'img/middle/10.png'},
+      {time: 4000, src: 'img/middle/11.png'},
+      {time: 4000, src: 'img/middle/12.png'},
     ];
     $scope.id = 0;
     $scope.MidImg = $scope.MiddlePictures[$scope.id].src;
@@ -88,20 +88,20 @@ $scope.id=0-1
 
   })
   .controller('proController', function ($scope) {
-    $scope.ProfessionalPictures = [{time: 3000, src: '/img/pro/1.png'},
-      {time: 1000, src: '/img/pro/2.png'},
-      {time: 2000, src: '/img/pro/3.png'},
-      {time: 3000, src: '/img/pro/4and10.png'},
-      {time: 3000, src: '/img/pro/5and11.png'},
-      {time: 4000, src: '/img/pro/6.png'},
-      {time: 4000, src: '/img/pro/7.png'},
-      {time: 4000, src: '/img/pro/8.png'},
-      {time: 4000, src: '/img/pro/9.png'},
-      {time: 4000, src: '/img/pro/12.png'},
-      {time: 4000, src: '/img/pro/13.png'},
-      {time: 4000, src: '/img/pro/14.png'},
-      {time: 4000, src: '/img/pro/15.png'},
-      {time: 4000, src: '/img/pro/16.png'},
+    $scope.ProfessionalPictures = [{time: 3000, src: 'img/pro/1.png'},
+      {time: 1000, src: 'img/pro/2.png'},
+      {time: 2000, src: 'img/pro/3.png'},
+      {time: 3000, src: 'img/pro/4and10.png'},
+      {time: 3000, src: 'img/pro/5and11.png'},
+      {time: 4000, src: 'img/pro/6.png'},
+      {time: 4000, src: 'img/pro/7.png'},
+      {time: 4000, src: 'img/pro/8.png'},
+      {time: 4000, src: 'img/pro/9.png'},
+      {time: 4000, src: 'img/pro/12.png'},
+      {time: 4000, src: 'img/pro/13.png'},
+      {time: 4000, src: 'img/pro/14.png'},
+      {time: 4000, src: 'img/pro/15.png'},
+      {time: 4000, src: 'img/pro/16.png'},
     ];
     $scope.id = 0;
     $scope.ProImg = $scope.ProfessionalPictures[$scope.id].src;
